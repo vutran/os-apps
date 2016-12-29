2cb7d2aeb817d9081c2f850f6d26f2652833587c
Vu Tran <vu@vu-tran.com>
2016-12-28 19:54:38 -0800

    inline some loops/funcs

--- index.js
+++ index.js
@@ -2,46 +2,30 @@ const fs = require('fs');
 const os = require('os');
 const path = require('path');
 
-const APP_DIRS = {
-  darwin: {
-    user: path.join(os.homedir(), 'Applications'),
-    system: path.join('/', 'Applications'),
-  },
-  win32: {
-    user: null,
-    system: [
-      // TODO: verify on win32 systems
-      process.env.ProgramFiles,
-      process.env['ProgramFiles(x86)'],
-    ],
-  },
-  linux: {
-    user: path.join('/', 'usr', 'share'),
-    system: path.join('/', 'usr', 'share'),
-  },
-};
-
 /**
  * Retrieves all apps in the given directory/directories
  *
- * @param {String|String[]} dirOrDirs - A directory or a list of directories to read
+ * @param {String[]} dirs - A list of directories to read
  * @return {Promise<string>} - Array of app paths
  */
-exports.getAppsInDirectories = dirOrDirs => new Promise((resolve) => {
+exports.getAppsInDirectories = dirs => new Promise((resolve) => {
   const results = [];
-  let dirsToRead = [];
-  if (typeof dirOrDirs === 'string') {
-    dirsToRead.push(dirOrDirs);
-  } else {
-    dirsToRead = dirOrDirs;
-  }
   // read all directories (collect all promises)
-  dirsToRead.forEach((dir) => {
+  for (let i = 0; i < dirs.length; i++) {
+    const dir = dirs[i];
     results.push(exports.getAppsInDirectory(dir));
-  });
+  }
   Promise.all(results)
     .then((dirResults) => {
-      const flatResults = dirResults.reduce((a, b) => a.concat(b), []);
+      const flatResults = [];
+      for (let i = 0; i < dirResults.length; i++) {
+        const dirFiles = dirResults[i];
+        if (dirFiles.length) {
+          for (let j = 0; j < dirFiles.length; j++) {
+            flatResults.push(dirFiles[j]);
+          }
+        }
+      }
       resolve(flatResults);
     });
 });
@@ -54,37 +38,20 @@ exports.getAppsInDirectories = dirOrDirs => new Promise((resolve) => {
  */
 exports.getAppsInDirectory = dir => new Promise((resolve) => {
   const results = [];
-  exports.readdir(dir).then((files) => {
-    files.forEach((file) => {
-      results.push(file);
-    });
-    resolve(results);
-  });
-});
-
-/**
- * Reads the directory recursively to the specified level
- *
- * @param {String} dir
- * @return {Promise<String[]>}
- */
-exports.readdir = dir => new Promise((resolve) => {
-  const results = [];
   fs.readdir(dir, (err, files) => {
     if (!err) {
-      files.forEach((file) => {
+      for (let i = 0; i< files.length; i++) {
+        const file = files[i];
         // set a flag to determine skipping a file
         let skipFile = false;
         // darwin systems should only return .app extensions
-        if (process.platform === 'darwin') {
-          if (path.extname(file) !== '.app') {
-            skipFile = true;
-          }
+        if (process.platform === 'darwin' && path.extname(file) !== '.app') {
+          skipFile = true;
         }
         if (!skipFile) {
           results.push(path.join(dir, file));
         }
-      });
+      }
     }
     resolve(results);
   });
@@ -96,4 +63,35 @@ exports.readdir = dir => new Promise((resolve) => {
  * @param {String} - The use type
  * @return {Promise<string>} - An array of app paths
  */
-exports.getAll = (useType = 'system') => exports.getAppsInDirectories(APP_DIRS[process.platform][useType]);
+exports.getAll = (useType = 'system') => {
+  let dirs = { user: '', system: '' };
+  switch (process.platform) {
+    case 'darwin':
+      dirs = {
+        user: path.join(os.homedir(), 'Applications'),
+        system: [
+          path.join('/', 'Applications'),
+        ],
+      };
+      break;
+    case 'win32':
+      dirs = {
+        user: null,
+        system: [
+          process.env.ProgramFiles,
+          process.env['ProgramFiles(x86)'],
+        ],
+      };
+      break;
+    case 'linux':
+      const usrSharePath = path.join('/', 'usr', 'share');
+      dirs = {
+        user: usrSharePath,
+        system: [
+          usrSharePath,
+        ],
+      };
+      break;
+  }
+  return exports.getAppsInDirectories(dirs[useType]);
+};
